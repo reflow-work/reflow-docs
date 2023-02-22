ab5ecbfebbeb1a51f4b23c97867c64b7c1c9e423
Jinkyou Son <nallwhy@gmail.com>
2023-02-23 08:49:21 +0900

    Update theme config

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,14 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  logo: <img src="/logo.svg" alt="Nextra Logo" width={120} />,
+  docsRepositoryBase: 'https://github.com/reflow-work/reflow-docs/tree/main',
   footer: {
-    text: 'Nextra Docs Template',
+    text: <span>
+      {new Date().getFullYear()} © <a href="https://reflow.work" target="_blank">reflow</a>.
+    </span>
   },
+  darkMode: false
 }
 
 export default config
